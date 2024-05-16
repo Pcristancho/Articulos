aacc9436699204600d1e481a034ff6924869d70a
Pcristancho <p.cristancho72@gmail.com>
2024-05-15 22:40:37 -0500

    se modifican descripciones con estilos y se añade enlaces de articulos

--- src/lista/Articulo.jsx
+++ src/lista/Articulo.jsx
@@ -1,19 +1,29 @@
 import estilosArticulo from './Articulo.module.css'
 function Articulo({...articuloMock}){
     const articulo={...articuloMock};
+    let descripcion=articulo.descripcion
+    // descripcion=JSON.parse(descripcion);
+    const tipo=descripcion.tipo;
+    const Genero=descripcion.Genero;
+    const Composicion=descripcion.Composicion;
+    const vectorDescripcion=[tipo,Genero,Composicion];
+
+    // descripcion=JSON.stringify(descripcion)
     return(
         <div className={estilosArticulo.contenedor}>
-            {/* <p>AQUI VA EL ARTICULO</p> */}
-            <div>
-                <img className={estilosArticulo.img} src={articulo.imgSrc}></img>
-            </div>
-            <div>
-                <p>{articulo.titulo}</p>
-                <p>{articulo.descripcion}</p>
-                <p>{articulo.precio}</p>
-                <p>{articulo.añoColeccion}</p>
-            </div>
-            
+            <a href={articulo.enlace} target='_blank'>
+                {/* <p>AQUI VA EL ARTICULO</p> */}
+                <div className={estilosArticulo.contenedorImg}>
+                    <img className={estilosArticulo.img} src={articulo.imgSrc}></img>
+                </div>
+                <div className='text-sm'>   
+                    <p>{articulo.titulo}</p>
+                    <p>{articulo.precio}</p>
+                    {vectorDescripcion.map((descrip,indice)=>(<span className={estilosArticulo.descripciones} key={indice}>{descrip}</span>))}
+                    
+                    <p>{articulo.añoColeccion}</p>
+                </div>
+            </a>
         </div>
     );
 }
--- src/servicios/Memoria.jsx
+++ src/servicios/Memoria.jsx
@@ -6,18 +6,41 @@ const ListaMock=[
     {
         "id":"1",
         "imgSrc":'./src/img/img1.png',
-        "titulo":"Saco 1",
-        "descripcion":"descripcion de saco 1",
-        "precio":"14.900",
-        "añoColeccion":"Nueva Colección"
+        "titulo":"Saco de Algodón University Club",
+        "descripcion":{
+            "tipo":"Sacos",
+            "Genero":"Hombre",
+            "Composicion":"Algodón"
+        },
+        "precio":"149.990",
+        "añoColeccion":"Nueva Colección",
+        "enlace":"https://www.falabella.com.co/falabella-co/product/882773332/Saco-para-Hombre-de-Algodon-Con-cremallera-University-Club/882773342"
     },
     {
         "id":"2",
-        "imgSrc":'./src/img/img2.png',
-        "titulo":"Saco 2",
-        "descripcion":"descripcion de saco 2",
-        "precio":"14.900",
-        "añoColeccion":"2007"
+        "imgSrc":'./src/img/img3.png',
+        "titulo":"Buzo Capucha Manga Larga",
+        "descripcion":{
+            "tipo":"Buzo",
+            "Genero":"Hombre",
+            "Composicion":"Algodón"
+        },
+        "precio":"125.940",
+        "añoColeccion":"2007",
+        "enlace":"https://www.urbenmood.com/buzo-saco-capucha-manga-larga-hombre-ajustado-1131"
+    },
+    {
+        "id":"3",
+        "imgSrc":'./src/img/img4.png',
+        "titulo":"Saco Artesanal Tejido",
+        "descripcion":{
+            "tipo":"Saco",
+            "Genero":"Hombre",
+            "Composicion":"Doble Hilo"
+        },
+        "precio":"22.900",
+        "añoColeccion":"2010",
+        "enlace":"https://ovejito.com/products/saco-artesanal-tejido-2"
     }
 ]
 const estadoInicial={
